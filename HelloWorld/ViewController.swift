72c87b75ea596b9a9853747d0c52841a9e27077f
Sergejs Tiselskis <slokas85@gmail.con>
2022-06-17 16:39:16 +0300

    buttonConfiguration

--- HelloWorld/ViewController.swift
+++ HelloWorld/ViewController.swift
@@ -17,13 +17,16 @@ class ViewController: UIViewController {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
         greetingLabel.isHidden = true
-        greetingButton.layer.cornerRadius = 15
+//        greetingButton.layer.cornerRadius = 15
+        greetingButton.configuration = setupButton(with: "Show Greeting")
     }
 
     @IBAction func greetingButtinTpped() {
         greetingLabel.isHidden.toggle()
-        greetingButton.setTitle(greetingLabel.isHidden ? "Show Greeting" : "Hide Greeting", for: .normal)
-        
+// финальна] оптимизация для сохрвния размера шрифта
+        greetingButton.configuration = setupButton(with: greetingLabel.isHidden ? "Show Greeting" : "Hide Greeting")
+//        greetingButton.setTitle(greetingLabel.isHidden ? "Show Greeting" : "Hide Greeting", for: .normal)
+    
         
 /*      этот код оптимизирован выше с тернарным оператором
         if greetingLabel.isHidden {
@@ -32,6 +35,16 @@ class ViewController: UIViewController {
             greetingButton.setTitle("Hide Greeting", for: .normal)
         } */
         
+    }
+    
+    private func setupButton(with title: String) -> UIButton.Configuration {
+        var buttonConfiguration = UIButton.Configuration.filled()
+        buttonConfiguration.baseBackgroundColor = #colorLiteral(red: 0.8078431487, green: 0.02745098062, blue: 0.3333333433, alpha: 1)
+        buttonConfiguration.title = title
+        buttonConfiguration.buttonSize = .large
+        buttonConfiguration.cornerStyle = .large
+        buttonConfiguration.attributedTitle?.font = UIFont.systemFont(ofSize: 24)
+        return buttonConfiguration
     }
     
 }
